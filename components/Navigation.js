9851ca8f48cf32e3a6aa73d715bf7701cbef14b3
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-10-10 22:27:44 +0300

    feat: create navigation
    Former-commit-id: dc45886aa9b79ea3f3985fb11a2b6857237f7b2c

---
+++ components/Navigation.js
@@ -0,0 +1,58 @@
+import { useState, useRef } from 'react';
+import Link from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navigation = ({ pages = [] }) => {
+  const [open, setOpen] = useState(false);
+  const anchor = useRef(null);
+
+  const handleOpenMenu = () => setOpen(true);
+  const handleCloseMenu = () => setOpen(false);
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Link href="/">
+          <Typography variant="h6" sx={{ flexGrow: 1, cursor: 'pointer' }}>
+            Fluffy
+          </Typography>
+        </Link>
+
+        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          {pages.map(el => (
+            <Link href={'/pages/' + el.slug} key={el.slug}>
+              <Button color="inherit">{el.title}</Button>
+            </Link>
+          ))}
+        </Box>
+
+        <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+          <Box ref={anchor} onClick={handleOpenMenu}>
+            <IconButton color="inherit">
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Menu open={open} onClose={handleCloseMenu} anchorEl={anchor.current} keepMounted>
+            {pages.map(el => (
+              <Link href={'/pages/' + el.slug} key={el.slug}>
+                <MenuItem>{el.title}</MenuItem>
+              </Link>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navigation;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
-const Home = () => <div />;
+import { Navigation } from 'components';
+import { getObjects } from 'lib/data';
+
+export const getServerSideProps = () => {
+  const pages = getObjects('pages', ['slug', 'title']);
+  return { props: { pages } };
+};
+
+const Home = ({ pages }) => <Navigation pages={pages} />;
 
 export default Home;
