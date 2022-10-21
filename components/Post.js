ba5d6446fe8033946dd2c0bc1309aa6b8e725edf
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-10-21 20:25:18 +0300

    (Post): improve layout

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,17 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 const Post = ({ title, content, thumbnail }) => (
   <Container sx={{ my: 4 }}>
-    <Typography variant="h1">{title}</Typography>
-    <img src={'/' + thumbnail + '?nf_resize=fit&w=900'} style={{ width: '100%' }} />
+    <h1>{title}</h1>
+    <Box
+      width={{ xs: 1, sm: 0.8, md: 0.5 }}
+      mr={{ xs: 0, sm: 'auto', md: 2 }}
+      ml={{ sm: 'auto', md: 0 }}
+      mb={{ xs: 0, md: 2 }}
+      sx={{ float: { md: 'left' } }}
+    >
+      <img src={'/' + thumbnail + '?nf_resize=fit&w=900'} style={{ width: '100%' }} />
+    </Box>
     <div dangerouslySetInnerHTML={{ __html: content }} />
   </Container>
 );
