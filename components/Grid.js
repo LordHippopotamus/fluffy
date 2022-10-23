bb01fe1b64cbbbdf312130a41a47b67ad5cce154
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-10-23 23:33:01 +0300

    fix: masonry spacing

--- components/Grid.js
+++ components/Grid.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const Grid = ({ list }) => (
   <Container sx={{ my: 8 }}>
-    <Masonry columns={{ xs: 1, md: 2 }} spacing={4}>
+    <Masonry columns={{ xs: 1, md: 2 }} spacing={4} sx={{ ml: '0px' }}>
       {list.map(el => (
         <Card {...el} key={el.slug} />
       ))}
