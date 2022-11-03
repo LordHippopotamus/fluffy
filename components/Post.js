d4c99a55ecddc4153c07d160549a51c86a3a1a66
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-11-03 18:15:36 +0300

    improve post layout

--- components/Post.js
+++ components/Post.js
@@ -1,15 +1,18 @@
-import { Box, Container, Stack, Typography } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
 import TagIcon from '@mui/icons-material/Tag';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const Post = ({ content, category, animal, date }) => (
   <Container>
     <Box dangerouslySetInnerHTML={{ __html: content }} />
-    <Box display="flex" justifyContent="space-between" my={4}>
-      <Stack direction="row" spacing={2}>
+    <Box display="flex" justifyContent="space-between" flexWrap="wrap" my={4}>
+      <Stack direction="row" alignItems="center" spacing={2}>
         <TagIcon color="secondary" /> {animal}
         <TagIcon color="secondary" /> {category}
       </Stack>
-      <Typography color="secondary">{date}</Typography>
+      <Stack direction="row" alignItems="center">
+        <CalendarMonthIcon color="secondary" /> {date}
+      </Stack>
     </Box>
   </Container>
 );
