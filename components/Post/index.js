6e112dab6bdbd1a59f71a527481c99cd471885ff
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-11-04 08:09:19 +0300

    feat(Post): add img styles

--- components/Post/index.js
+++ components/Post/index.js
@@ -1,10 +1,11 @@
 import { Box, Container, Stack } from '@mui/material';
 import TagIcon from '@mui/icons-material/Tag';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import styles from './post.module.css';
 
 const Post = ({ content, category, animal, date }) => (
   <Container>
-    <Box dangerouslySetInnerHTML={{ __html: content }} />
+    <Box className={styles.post} dangerouslySetInnerHTML={{ __html: content }} />
     <Box display="flex" justifyContent="space-between" flexWrap="wrap" my={4}>
       <Stack direction="row" alignItems="center" spacing={2}>
         <TagIcon color="secondary" /> {animal}
